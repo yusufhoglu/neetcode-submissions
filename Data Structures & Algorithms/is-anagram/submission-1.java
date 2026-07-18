665439531cbb8e64729e3c1c81c837c69cf71ee3
hüseyin yusuf hocaoğlu <92581843+yusufhoglu@users.noreply.github.com>
2026-07-18 15:58:31 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false; 
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) < 0)
+                return false;
+        }
+        return true;
+    }
+}
