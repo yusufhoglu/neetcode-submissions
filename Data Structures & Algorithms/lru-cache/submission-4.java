541797b6ddc3826f9f662a9caf615d0a365f59bc
hüseyin yusuf hocaoğlu <92581843+yusufhoglu@users.noreply.github.com>
2026-07-18 17:01:39 +0300

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.java
@@ -0,0 +1,21 @@
+class LRUCache {
+    LinkedHashMap<Integer, Integer> map;
+    int capacity;
+    public LRUCache(int capacity) {
+        this.map = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
