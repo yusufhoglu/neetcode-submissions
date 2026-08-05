c321fc94a372159a912ec5be4244e4f6ed3e9937
hüseyin yusuf hocaoğlu <92581843+yusufhoglu@users.noreply.github.com>
2026-08-05 22:47:23 +0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int L = 0;
+        int R = numbers.length - 1;
+
+        while (R > L) {
+            int total = numbers[R] + numbers[L];
+            if (total > target) {
+                R--;
+            } else if(total < target) {
+                L++;
+            } else {
+                return new int[]{L + 1, R + 1};
+            }
+        }
+        return new int[0];
+    }
+}
