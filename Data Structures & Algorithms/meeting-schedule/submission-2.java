e6b1f3295af50dd803910cb1f001a91628e74d5e
hüseyin yusuf hocaoğlu <92581843+yusufhoglu@users.noreply.github.com>
2026-09-18 22:57:04 +0300

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval i1 = intervals.get(i - 1);
+            Interval i2 = intervals.get(i);
+
+            if (i1.end > i2.start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
